a374b000e8d16c382c0d768b1392805243854cf7
Tirth <tirthvshah7@gmail.com>
2021-08-13 21:28:13 +0530

    EJS configured

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const webtorrent=require("./routes/webtorrent");
 const showwebpage=require("./routes/showwebsite");
+const frontend = require("./routes/frontend")
+const path = require("path")
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/hostnew',webtorrent);
 app.use('/data',showwebpage);
+app.use('/frontend', frontend)
 const PORT = 5000;
 app.listen(PORT, () => {
 	console.log("Server started ");
