842e99e0f98ff3c686acde0fdb513a1821296771
akash <akash.ry@somaiya.edu>
2021-08-13 20:49:35 +0530

    resolve merge conflict

--- routes/webtorrent.js
+++ routes/webtorrent.js
@@ -5,18 +5,20 @@ const fse = require('fs-extra');
 var client = new WebTorrent();
 const fs = require('fs');
 router.get('/:id', async (req, res) => {
+    var Id = req.params.id;
     try {
-        if (!fs.existsSync(path.join(__dirname, '..', 'data', req.params.id))) {
-            client.add('magnet:?xt=urn:btih:' + req.params.id + '&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com', function (torrent) {
-                if (!fs.existsSync(path.join(__dirname, '..', 'data', req.params.id))) {
-                    fs.mkdir(path.join(__dirname, '..', 'data', req.params.id), (err) => {
+        if (!fs.existsSync(path.join(__dirname, '..', 'data', Id))) {
+            client.add('magnet:?xt=urn:btih:' + Id + '&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com', function (torrent) {
+                console.log("Get it");
+                if (!fs.existsSync(path.join(__dirname, '..', 'data', Id))) {
+                    fs.mkdir(path.join(__dirname, '..', 'data', Id), (err) => {
                         if (err) {
                             return console.error(err);
                         }
                         torrent.files.forEach(element => {
                             element.getBuffer(function (err, buffer) {
                                 if (err) throw err
-                                fs.appendFile(path.join(__dirname, '..', 'data', req.params.id, element.name), buffer, function (err) {
+                                fs.appendFile(path.join(__dirname, '..', 'data', Id, element.name), buffer, function (err) {
                                     if (err) throw err;
                                     console.log('Saved!');
                                 });
@@ -24,10 +26,10 @@ router.get('/:id', async (req, res) => {
                         });
                     });
                 }
-                res.sendFile(path.join(__dirname, '..', 'data', req.params.id, 'index.html'));
+                res.sendFile(path.join(__dirname, '..', 'data', Id, 'index.html'));
             });
         } else {
-            res.redirect(`http://localhost:5000/data/${req.params.id}/`);
+            res.redirect(`http://localhost:5000/data/${Id}/`);
         }
     } catch (error) {
         res.send(error);
