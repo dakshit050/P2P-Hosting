8b939ed00d6a193e977c16e3965317483b003ca3
akash <akash.ry@somaiya.edu>
2021-08-13 21:18:55 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const webtorrent=require("./routes/webtorrent");
-const showwebpage=require("./routes/showwebsite");
-app.use('/hostnew',webtorrent);
-app.use('/data',showwebpage);
+const webtorrent = require("./routes/webtorrent");
+const showwebpage = require("./routes/showwebsite");
+app.use('/hostnew', webtorrent);
+app.use('/data', showwebpage);
 const PORT = 5000;
 app.listen(PORT, () => {
 	console.log("Server started ");
