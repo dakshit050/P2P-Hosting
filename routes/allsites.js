bebabefec8fa175c4cad5476f63501d0e6c733ef
akash-yadav12 <akash.ry@somaiya.edu>
2022-03-03 21:37:45 +0530

    complete UI done

--- routes/allsites.js
+++ routes/allsites.js
@@ -3,16 +3,22 @@ const AllSites = require("../models/websites.model");
 
 const router = require("express").Router();
 router.get("/", async function (req, res) {
-	const allSites = await AllSites.find();
-	res.render("home", {
-		data: allSites,
-	});
+  const allSites = await AllSites.find();
+  const currentUser = await Users.findById({ _id: req.session.passport.user });
+  res.render("home", {
+    data: allSites,
+    currentUser: currentUser,
+    isMySites: false,
+  });
 });
 
 router.get("/mysites", async function (req, res) {
-	const MySites = await AllSites.find({ "owner": req.session.passport.user });
-	res.render("home", {
-		data: MySites,
-	});
+  const MySites = await AllSites.find({ owner: req.session.passport.user });
+  const currentUser = await Users.findById({ _id: req.session.passport.user });
+  res.render("home", {
+    data: MySites,
+    currentUser: currentUser,
+    isMySites: true,
+  });
 });
 module.exports = router;
